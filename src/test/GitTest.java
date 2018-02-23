421dd7ecf80510ac3ad0f483b9fc7688abc1e3a2
lv941030 <lv941030@gmail.com>
2018-02-23 16:06:34 +0800

    这次增加了一个test包来测试可不可以提交新的修改

---
+++ src/test/GitTest.java
@@ -0,0 +1,12 @@
+package test;
+
+/**
+ * @Description:测试Idea能不能增加文件
+ * @author: Olliween
+ * @date: 2018/02/2018/2/23 16:03
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("HelloWorld");
+    }
+}
